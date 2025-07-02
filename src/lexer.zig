0a4400fb2d5099d49d5ea604feedbe6d6d860b9b
Andre Ribeiro <andresmribeiro@gmail.com>
2025-07-02 23:29:02 +0100

    feat: initial commit with lexer stuff

---
+++ src/lexer.zig
@@ -0,0 +1,80 @@
+const std = @import("std");
+const token = @import("token.zig");
+
+const Lexer = struct {
+    input: []const u8,
+    position: u32,
+    read_position: u32,
+    ch: u8,
+
+    fn new(input: []const u8) Lexer {
+        var l: Lexer = .{
+            .input = input,
+            .position = 0,
+            .read_position = 0,
+            .ch = '0',
+        };
+        l.read_char();
+        return l;
+    }
+
+    fn read_char(self: *Lexer) void {
+        if (self.read_position >= self.input.len) {
+            self.ch = '0';
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    fn next_token(self: *Lexer) token.Token {
+        var tok: token.Token = .{
+            .type = token.TokenType.illegal,
+            .literal = '0',
+        };
+
+        switch (self.ch) {
+            '=' => tok = .{ .type = token.TokenType.assign, .literal = self.ch },
+            ';' => tok = .{ .type = token.TokenType.semicolon, .literal = self.ch },
+            '(' => tok = .{ .type = token.TokenType.l_paren, .literal = self.ch },
+            ')' => tok = .{ .type = token.TokenType.r_paren, .literal = self.ch },
+            '{' => tok = .{ .type = token.TokenType.l_brace, .literal = self.ch },
+            '}' => tok = .{ .type = token.TokenType.r_brace, .literal = self.ch },
+            ',' => tok = .{ .type = token.TokenType.comma, .literal = self.ch },
+            '+' => tok = .{ .type = token.TokenType.plus, .literal = self.ch },
+            else => tok = .{ .type = token.TokenType.eof, .literal = '0' },
+        }
+
+        self.read_char();
+        return tok;
+    }
+};
+
+test "test next token" {
+    const input = "=+(){},;";
+
+    var tests = std.ArrayList(token.Token).init(std.testing.allocator);
+    defer tests.deinit();
+
+    try tests.append(.{ .type = token.TokenType.assign, .literal = '=' });
+    try tests.append(.{ .type = token.TokenType.plus, .literal = '+' });
+    try tests.append(.{ .type = token.TokenType.l_paren, .literal = '(' });
+    try tests.append(.{ .type = token.TokenType.r_paren, .literal = ')' });
+    try tests.append(.{ .type = token.TokenType.l_brace, .literal = '[' });
+    try tests.append(.{ .type = token.TokenType.r_brace, .literal = ']' });
+    try tests.append(.{ .type = token.TokenType.comma, .literal = ',' });
+    try tests.append(.{ .type = token.TokenType.semicolon, .literal = ';' });
+    try tests.append(.{ .type = token.TokenType.eof, .literal = '0' });
+
+    var l = Lexer.new(input);
+
+    for (tests.items) |t| {
+        const tok = l.next_token();
+
+        try std.testing.expectEqual(token.TokenType, t.type, tok.type);
+        try std.testing.expectEqual([]const u8, t.literal, tok.literal);
+        try std.testing.expectEqual([]const u8, t.literal, "asd");
+    }
+}
---
+++ src/token.zig
@@ -0,0 +1,45 @@
+pub const TokenType = enum {
+    illegal,
+    eof,
+
+    // identifiers + literals
+    ident,
+    int,
+
+    // operators
+    assign,
+    plus,
+
+    // delimiters
+    comma,
+    semicolon,
+
+    l_paren,
+    r_paren,
+    l_brace,
+    r_brace,
+
+    // keywords
+    function,
+    let,
+
+    pub fn name(self: TokenType) []const u8 {
+        return switch (self) {
+            .eof => "EOF",
+            .assign => "=",
+            .plus => "+",
+            .comma => ",",
+            .semicolon => ";",
+            .l_paren => "(",
+            .r_paren => ")",
+            .l_brace => "[",
+            .r_brace => "]",
+            .function => "FUNCTION",
+            .let => "LET",
+
+            else => "ILLEGAL",
+        };
+    }
+};
+
+pub const Token = struct { type: TokenType, literal: u8 };
